a215e616588818f578b3a7c6d98242691336367c
matt <mmoss098@gmail.com>
2015-08-03 14:50:36 +0100

    moved all question components into one file

--- src/views/question-container.js
+++ src/views/question-container.js
@@ -4,9 +4,10 @@
  */
 'use strict';
 var React = require('react');
-var QuestionView = require('./components/question');
-var Query = require('./components/query');
-var Info = require('./components/info');
+var Choice = require('./components/choice');
+//var QuestionView = require('./components/question');
+//var Query = require('./components/query');
+//var Info = require('./components/info');
 var ampersandMixin = require('ampersand-react-mixin');
 
 module.exports = React.createClass({
@@ -29,12 +30,21 @@ module.exports = React.createClass({
       
     return {
       choices: choices,
-      text: ''
+      text: '',
+      query: this.props.survey.get(lastQuestion).query,
+      info: this.props.survey.get(lastQuestion).info
     };
   },
   
+  // handlers
+  onQueryChange: function(event) {
+    this.setState({
+      query: event.target.value
+    });
+  },
+  
     // method to add a new empty question
-  onSaveClick: function() {
+  onSaveQuestionClick: function() {
     // alert('new question'); // test purposes
     // save the current question to the current model
     // this may already be implemented I think...yes it happens when you type
@@ -48,21 +58,164 @@ module.exports = React.createClass({
     var newChoices = this.props.survey.get(lastQuestion).choices;
     this.replaceState({ choices: newChoices });
   },
+  
+  //save button
+  onQuerySubmit: function(event) {
+    event.preventDefault();
+    //alert('just checking!'); // testing
+    var lastQuestion = this.props.survey.length;
+    var currentQuestion = this.props.survey.get(lastQuestion);
+    currentQuestion.query = this.state.query;
+    currentQuestion.editQuery = false;
+  },
+  // edit button
+  onEditQueryClick: function(event) {
+    event.preventDefault();
+    var lastQuestion = this.props.survey.length;
+   // alert('editing'); // for testing
+    this.props.survey.get(lastQuestion).editQuery = true;
+  },
+  
+  
+  onInfoChange: function(event) {
+    this.setState({
+      info: event.target.value
+    });
+  },
+  
+  onInfoSubmit: function(event) {
+    event.preventDefault();
+    var lastQuestion = this.props.survey.length;
+    var currentQuestion = this.props.survey.get(lastQuestion);
+    currentQuestion.info = this.state.info;
+    currentQuestion.editInfo = false;
+  },
+  
+  onEditInfoClick: function(event) {
+    event.preventDefault();
+    var lastQuestion = this.props.survey.length;
+    this.props.survey.get(lastQuestion).editInfo = true;
+  },
+  
+  
+ // when you type state updates
+  onChange: function(event) {
+    this.setState({text: event.target.value});
+  },
+  // when button is clicked Array.concat() creates & returns new array with
+  // original contents plus what was typed in
+  // then state is set to the new array and empty text string
+  onAddChoice: function(event) {
+    event.preventDefault();
+    var lastQuestion = this.props.survey.length;
+    var nextChoices = this.state.choices.concat([this.state.text]);
+    // assign it to the model
+    this.props.survey.get(lastQuestion).choices = nextChoices;
+    var nextText = '';
+    this.setState({choices: nextChoices, text: nextText});
+  },
+  
+  // remove a choice - currently from start of array - needs updating to ask
+  // user which choice they wish to remove***this is done BUT if user does not enter a number it still deletes choice at index zero - needs fixing**********
+  // using Array.splice()
+  onDeleteChoice: function(index) {
+    var lastQuestion = this.props.survey.length;
+    var index = prompt('Please enter the choice index to delete');
+    // if user enters an index delete that one
+    if (index != null) {
+    // copy array
+    var choicesCopy = this.state.choices.slice();
+    // remove element at index - in state
+    choicesCopy.splice(index, 1);
+    // assign it to the model 
+    this.props.survey.get(lastQuestion).choices = choicesCopy;
+    // update the state
+    this.setState({ choices: choicesCopy });
+    };
+      
+    // if they click cancel - do nothing      
 
+  },
+  
   
   render: function() {
     var lastQuestion = this.props.survey.length;
-
+    var queryContent;
+    
+    if (this.props.survey.get(lastQuestion).editQuery === true) {
+      // Edit mode
+      queryContent = (
+        <form onSubmit={this.onQuerySubmit}>
+          <fieldset>
+            <legend>Query</legend>
+            <input type='text' name='query' value={this.state.query}
+              onChange={this.onQueryChange} />
+            <button type='submit'>
+              Save
+            </button>
+          </fieldset>
+        </form>
+      );
+    } else {
+      // display mode
+      queryContent = (
+        <div>
+          <span>{this.props.survey.get(lastQuestion).query}</span>
+          <span>
+            <button onClick={this.onEditQueryClick} type='button'>
+              Edit
+            </button>
+          </span>
+        </div>
+      );
+    }
+    
+    var infoContent;
+    
+    if (this.props.survey.get(lastQuestion).editInfo === true) {
+      infoContent = (
+        <form onSubmit={this.onInfoSubmit}>
+          <fieldset>
+            <legend>Question Info</legend>
+            <textarea value={this.state.info} onChange={this.onInfoChange} />
+            <button type='submit'>Save</button>
+          </fieldset>
+        </form>
+      );
+    } else {
+      infoContent = (
+        <div>
+          <span>{this.props.survey.get(lastQuestion).info}</span>
+        
+          <button onClick={this.onEditInfoClick} type='button'>Edit</button>
+        </div>
+      );
+    }
+    
     return (
-      <fieldset>
+      <div>
         <legend>Questions will display here</legend>
-        <Query survey={this.props.survey} />
-        <Info survey={this.props.survey} />
-        <QuestionView key={this.state.text} survey={this.props.survey} choices={this.state.choices} />
-              <button type='button' onClick={this.onSaveClick}>
-          Save Current & Add New Question
+    
+        {queryContent}
+  
+    
+        {infoContent}
+    <div>
+        <Choice choices={this.state.choices} />
+
+          <input onChange={this.onChange} value={this.state.text} />
+          <button type='button' onClick={this.onAddChoice}>
+            Add Choice</button>
+          <button type='button' onClick={this.onDeleteChoice}>
+            Delete Choice</button>
+      </div>
+              <button type='button' onClick={this.onSaveQuestionClick}>
+          Save Question
         </button>
-      </fieldset>
+                    <button type='button' >
+          New Question
+        </button>
+      </div>
     );
   }
 });
