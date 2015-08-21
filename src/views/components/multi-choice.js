549c2b82ca8294628b3318a5222f1c925ae18bf5
matt <mmoss098@gmail.com>
2015-08-21 12:23:01 +0100

    multi-choice refactored to seperate component & state for it left in question-container - minor issue with new question button state to be sorted later

--- src/views/components/multi-choice.js
+++ src/views/components/multi-choice.js
@@ -3,19 +3,103 @@
  */
 'use strict';
 var React = require('react');
+var Choice = require('./choice');
+//var ampersandMixin = require('ampersand-react-mixin');
 
 module.exports = React.createClass({
   displayName: 'MultipleChoice',
+  //mixins: [ampersandMixin],
   
   getInitialState: function() {
+
+    
     return {
-      
+      choices: this.props.choices,
+      text: ''
     }
   },
   
+  // multiple choice question stuff
+  // start with empty text string - choices is an array of strings
+  onTextChange: function(event) {
+    this.setState({text: event.target.value});
+  },
+  // when button is clicked Array.concat() creates & returns new array with
+  // original contents plus what was typed in
+  // then state is set to the new array and empty text string
+  onAddChoice: function(event) {
+    event.preventDefault();
+    var lastQuestion = this.props.user.survey.length;
+    var nextChoices = this.state.choices.concat([this.state.text]);
+    // assign it to the model
+    this.props.user.survey.get(lastQuestion).choices = nextChoices;
+    var nextText = '';
+    this.setState({choices: nextChoices, text: nextText});
+  },
+  // remove a choice - BUT if user does not
+  // enter a number it still deletes choice at index zero - needs fixing*******
+  // using Array.splice()
+  onDeleteChoice: function(index) {
+    var lastQuestion = this.props.user.survey.length;
+    var index = prompt('Please enter the answer choice number to delete');
+    // if user enters an index delete that one
+    if (index != null) {
+      // copy array
+      var choicesCopy = this.state.choices.slice();
+      // remove element at index - in state
+      choicesCopy.splice(index, 1);
+      // assign it to the model
+      this.props.user.survey.get(lastQuestion).choices = choicesCopy;
+      // update the state
+      this.setState({choices: choicesCopy});
+    }
+    // if they click cancel - do nothing
+  },
+  
+  onSaveChoicesClick: function() {
+   // alert('Saving!');
+    this.props.user.editChoices = false;
+  },
+  
+    
+  onEditChoicesClick: function(event) {
+    event.preventDefault();
+    this.props.user.editChoices = true;
+  },
+  
   render: function() {
+    var lastQuestion = this.props.user.survey.length;
+
+    // multiple choice question view
+    var choicesContent;
+    if (this.props.user.editChoices === true) {
+      choicesContent = (
+                <div>
+          <Choice choices={this.state.choices} />
+          <label htmlFor='answer-text'>Enter answer choice</label>
+          <input type='text' onChange={this.onTextChange} value={this.state.text} id='answer-text' className='form-input'/>
+          <div className='button-group'>
+          <button type='button' onClick={this.onAddChoice} className='button'>Add Choice</button>
+          <button type='button' onClick={this.onDeleteChoice} className='button'>
+            Delete Choice
+          </button>
+          </div>
+        </div>
+    );
+    } else {
+      choicesContent = (
+      <fieldset>
+        <Choice choices={this.state.choices} />
+          <button onClick={this.onEditChoicesClick} type='button' className='button pull-right'>Edit</button>
+      </fieldset>
+    );
+    }
     return (
       <div>
+        {choicesContent}
+       <button type='button' onClick={this.onSaveChoicesClick} className='button'>
+          Save Choices
+        </button>
       </div>    
     );
   }
--- src/views/question-container.js
+++ src/views/question-container.js
@@ -4,87 +4,41 @@
  */
 'use strict';
 var React = require('react');
-var Choice = require('./components/choice');
+
 var Query = require('./components/query');
 var Info = require('./components/info');
-
+var MultiChoice = require('./components/multi-choice');
+//var ampersandMixin = require('ampersand-react-mixin');
 // this file needs refactoring into seperate components with individual concerns
 module.exports = React.createClass({
   displayName: 'QuestionContainer',
-  
+ // mixins: [ampersandMixin],
   getInitialState: function() {
-    var lastQuestion = this.props.survey.length;
+    
+    var lastQuestion = this.props.user.survey.length;
     var choices;
     // check if any question models already exist
-    if (typeof this.props.survey.get(lastQuestion) != 'undefined') {
+    if (typeof this.props.user.survey.get(lastQuestion) != 'undefined') {
       // if they do set the last one's choices to choices var
-      choices = this.props.survey.get(lastQuestion).choices;
+      choices = this.props.user.survey.get(lastQuestion).choices;
       //alert('already exists!');
     } else {
       // if not create a new question model & set its choices to choices var
-      this.props.survey.add({ });
+      this.props.user.survey.add({ });
       var lastQuestion = app.user.survey.length;
       choices = app.user.survey.get(lastQuestion).choices;
       //alert('added one for ya!');
     }
     
     return {
-
-
-      choices: choices,
-      text: ''
+      choices: choices
     }
   },
-  // multiple choice question stuff
-  // start with empty text string - choices is an array of strings
-  onTextChange: function(event) {
-    this.setState({text: event.target.value});
-  },
-  // when button is clicked Array.concat() creates & returns new array with
-  // original contents plus what was typed in
-  // then state is set to the new array and empty text string
-  onAddChoice: function(event) {
-    event.preventDefault();
-    var lastQuestion = this.props.survey.length;
-    var nextChoices = this.state.choices.concat([this.state.text]);
-    // assign it to the model
-    this.props.survey.get(lastQuestion).choices = nextChoices;
-    var nextText = '';
-    this.setState({choices: nextChoices, text: nextText});
-  },
-  // remove a choice - BUT if user does not
-  // enter a number it still deletes choice at index zero - needs fixing*******
-  // using Array.splice()
-  onDeleteChoice: function(index) {
-    var lastQuestion = this.props.survey.length;
-    var index = prompt('Please enter the answer choice number to delete');
-    // if user enters an index delete that one
-    if (index != null) {
-      // copy array
-      var choicesCopy = this.state.choices.slice();
-      // remove element at index - in state
-      choicesCopy.splice(index, 1);
-      // assign it to the model
-      this.props.survey.get(lastQuestion).choices = choicesCopy;
-      // update the state
-      this.setState({choices: choicesCopy});
-    }
-    // if they click cancel - do nothing
-  },
-
 
   // Save & New buttons
-  onSaveQuestionClick: function() {
-   // alert('Saving!');
-    this.props.user.editChoices = false;
-  },
-  
-  onEditChoicesClick: function(event) {
-    event.preventDefault();
-    this.props.user.editChoices = true;
-  },
-  
-  onNewQuestionClick: function() {
+
+
+  onNewQuestionClick: function(event) {
     //alert('Newing!');
     // need validation of current Q data
     // add a new empty question model to the collection
@@ -92,9 +46,12 @@ module.exports = React.createClass({
     // make the new question the current state
     var lastQuestion = this.props.survey.length;
     var newChoices = this.props.survey.get(lastQuestion).choices;
+    // just need to sort this - choices is now in child - multi-choice
     this.replaceState({choices: newChoices});
+    console.log(this.state.choices);
     this.props.user.editQuery = true;
     this.props.user.editInfo = true;
+    this.props.user.editChoices = true;
   },
   
   render: function() {
@@ -102,31 +59,6 @@ module.exports = React.createClass({
     // question number for display
     var currentQuestion = this.props.survey.get(lastQuestion).questionNumber;
 
-
-    // multiple choice question view
-    var choicesContent;
-    if (this.props.user.editChoices === true) {
-      choicesContent = (
-                <div>
-          <Choice choices={this.state.choices} />
-          <label htmlFor='answer-text'>Enter answer choice</label>
-          <input type='text' onChange={this.onTextChange} value={this.state.text} id='answer-text' className='form-input'/>
-          <div className='button-group'>
-          <button type='button' onClick={this.onAddChoice} className='button'>Add Choice</button>
-          <button type='button' onClick={this.onDeleteChoice} className='button'>
-            Delete Choice
-          </button>
-          </div>
-        </div>
-    );
-    } else {
-      choicesContent = (
-      <fieldset>
-        <Choice choices={this.state.choices} />
-          <button onClick={this.onEditChoicesClick} type='button' className='button pull-right'>Edit</button>
-      </fieldset>
-    );
-    }
   
     return (
       <div className='grid-flex-cell'>
@@ -134,11 +66,9 @@ module.exports = React.createClass({
         <span>Question {currentQuestion}</span>
         <Query user={this.props.user} />
         <Info user={this.props.user} />
-        {choicesContent}
+        <MultiChoice user={this.props.user} choices={this.state.choices} />
         <div className='button-group pull-right'>
-        <button type='button' onClick={this.onSaveQuestionClick} className='button'>
-          Save Question
-        </button>
+ 
         <button type='button' onClick={this.onNewQuestionClick} className='button'>
           New Question
         </button>
